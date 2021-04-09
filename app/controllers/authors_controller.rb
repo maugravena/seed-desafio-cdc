2688075ec41968f6a8c216a9b533f4cd3d5c88d9
Mauricio Gravena <mauricio.gravena@rebase.com.br>
2021-04-08 23:16:34 -0300

    Create author and returns status 200

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,12 @@
+class AuthorsController < ApplicationController
+  def create
+    @author = Author.create!(author_params)
+    render json: @author, status: :created
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :email, :description)
+  end
+end
