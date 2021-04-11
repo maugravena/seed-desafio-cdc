ba3ceb2533b1bd1947bb62ebff21cd77e0112c8f
Mauricio Gravena <mauricio.gravena@rebase.com.br>
2021-04-11 17:01:14 -0300

    Handle RecordInvalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: {
+      message: e.message
+    }, status: :precondition_failed
+  end
 end
